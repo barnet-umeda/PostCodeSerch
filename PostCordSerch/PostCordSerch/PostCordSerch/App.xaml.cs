a9ed2a21a23a93a33d620e5e2d6ee919f094fe58
Shinya.Umeda <umeda@barnet.co.jp>
2022-12-24 10:33:06 +0900

    プロジェクト再構築

---
+++ PostCordSerch/PostCordSerch/PostCordSerch/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PostCordSerch;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PostCordSerch/PostCordSerch/PostCordSerch/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PostCordSerch;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
