e0d73e52b4d115176d2920997267d373f3a992c8
Shinya.Umeda <umeda@barnet.co.jp>
2022-12-25 13:26:06 +0900

    no message

--- PostCordSerch/PostCordSerch/PostCordSerch/PageSerch.xaml.cs
+++ PostCordSerch/PostCordSerch/PostCordSerch/PageSerch.xaml.cs
@@ -1,14 +1,49 @@
+using System.Text.Json.Nodes;
+
 namespace PostCordSerch;
 
 public partial class PageSerch : ContentPage
 {
-	public PageSerch()
+    
+    public PageSerch()
 	{
 		InitializeComponent();
 	}
 
-    private void ClickedButtonSerch(object sender, EventArgs e)
+    private async void ClickedButtonSerch(object sender, EventArgs e)
     {
+        if (EntryPostCode.Text == null || EntryPostCode.Text.Length < 7)
+        {
+            await DisplayAlert("", "郵便番号を入力してください", "OK");
+            return;
+        }
 
+        HttpClient client = new HttpClient();
+        JsonNode PostInfo;  //Httpレスポンス
+
+        string ApiAddress = $"https://zipcloud.ibsnet.co.jp/api/search?zipcode={EntryPostCode.Text}";
+
+        try
+        {
+            client.Timeout = TimeSpan.FromSeconds(5); //タイムアウトを5秒に設定
+            using HttpResponseMessage response = await client.GetAsync(ApiAddress);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            PostInfo = JsonNode.Parse(responseBody);
+            Console.WriteLine(PostInfo);
+
+            if (PostInfo["results"] == null) throw new Exception("error");
+
+            JsonNode jnBuf = PostInfo["results"];
+            JsonArray jaBuf = (JsonArray)PostInfo["results"];
+            JsonValue jvBuf = (JsonValue)jaBuf[0]["address1"];
+
+            await DisplayAlert("", jvBuf.ToString(), "OK");
+        }
+        catch
+        {
+            await DisplayAlert("", "取得に失敗しました", "OK");
+        }
     }
 }
